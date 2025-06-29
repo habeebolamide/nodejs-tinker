b37d34b63bc6ab8cdce799f1140030001f2ac9c2
Habeeb <mac@MACs-MacBook-Pro.local>
2025-06-29 14:29:26 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+require('dotenv').config();
